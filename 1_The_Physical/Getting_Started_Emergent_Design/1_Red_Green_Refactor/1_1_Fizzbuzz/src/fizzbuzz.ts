9e7661a424491bfe338053ebd1a4ea715003ab96
Matt Wallin <matt.wallin2@gmail.com>
2023-08-30 12:31:00 -0700

    Function only works when number is between 1 and 100

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,11 +1,14 @@
 export default function fizzbuzz(num: number): string {
-    if(num % 5 == 0 && num % 3 == 0) {
+    if(num < 1 || num > 100) {
+        return 'Number can not be higher than 100 or lower than 1'
+    }
+    else if(num % 5 == 0 && num % 3 == 0) {
         return "fizzbuzz";
     }
-    if(num % 3 == 0) {
+    else if(num % 3 == 0) {
         return "fizz"
     }
-    if(num % 5 == 0) {
+    else if(num % 5 == 0) {
         return "buzz"
     }
     return "" + num;
--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,6 +1,6 @@
 import fizzbuzz from "./fizzbuzz";
 
-
+//Have the function throw an error if num is < 1 or > 100
 
 describe("fizzbuzz", () => {
     it.each([
@@ -9,7 +9,9 @@ describe("fizzbuzz", () => {
         [3, 'fizz'],
         [5, 'buzz'],
         [15, 'fizzbuzz'],
-        [45, 'fizzbuzz']
+        [45, 'fizzbuzz'],
+        [0, 'Number can not be higher than 100 or lower than 1'],
+        [101, 'Number can not be higher than 100 or lower than 1']
     ])('entered: %i, expected: %s', (num, expected) => {
         expect(fizzbuzz(num)).toBe(expected);
     });
