d89044896beebc07f1fc57ae9633bbd014c40de0
Matt Wallin <matt.wallin2@gmail.com>
2023-09-01 14:32:26 -0700

    Refactored

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,8 +1,7 @@
 export default function palindromeChecker(str: string): boolean {
     const str1 = str.toLocaleLowerCase().split(' ').join(''); //str1 removes spaces and capital letters
     const str2 = str1.split('').reverse().join(''); //str2 reverses str1 for comparison
-    if(str1 === str2) {
+    if(str1 === str2)
         return true;
-    }
     return false;
 }
