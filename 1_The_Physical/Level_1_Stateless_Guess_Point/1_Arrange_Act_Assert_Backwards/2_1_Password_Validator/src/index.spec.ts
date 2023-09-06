4c27ca7fee0e1f17bfa341eea3e8adc57a30e5b3
Matt Wallin <matt.wallin2@gmail.com>
2023-09-06 16:41:31 -0700

    Checks if the password contains a digit

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -2,19 +2,22 @@ import passwordvalidator from "./index"
 
 describe('password validator', () => {
   it.each([
-    "12345",
-])('knows that %s IS a valid password', (str) => {
-    expect(passwordvalidator(str)).toHaveProperty('isValid', true);
-});
+    "1234",
+    "1234567890123456"
+  ])('knows that %s IS NOT within 5 and 15 characters', (str) => {
+    expect(passwordvalidator(str)).toHaveProperty('isValid', false);
+    expect(passwordvalidator(str)).toHaveProperty('errors', ["Password must be within 5 and 15 characters"]);
 
-it.each([
-  "1234",
-  "1234567890123456"
-])('knows that %s IS NOT a valid password', (str) => {
-  expect(passwordvalidator(str)).toHaveProperty('isValid', false);
-  expect(passwordvalidator(str)).toHaveProperty('errors', ["Password must be within 5 and 15 characters"]);
+  });
 
-});
+  it.each([
+    "Roots",
+    "Joker",
+  ])('knows that %s DOES NOT contain a digit', (str) => {
+    expect(passwordvalidator(str)).toHaveProperty('isValid', false);
+    expect(passwordvalidator(str)).toHaveProperty('errors', ["Password must contain a digit"]);
+
+  });
 })
 
 
--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -3,13 +3,18 @@ interface PasswordValidator {
     errors: String[] | undefined;
 }
 
-export default function passwordValidator(password: string): PasswordValidator {
+export default function passwordValidator(passwordStr: string): PasswordValidator {
     let isValid = true;
     let errorsArr: String[] = [];
+    let password = passwordStr.split('');
 
-    if(password.length < 5 || password.length > 15) {
+    if(passwordStr.length < 5 || passwordStr.length > 15) {
         isValid = false;
         errorsArr.push("Password must be within 5 and 15 characters")
+    }
+    else if(!(password.includes("/\d/"))) {
+        isValid = false;
+        errorsArr.push("Password must contain a digit")
     }
     return {"isValid": isValid, "errors": errorsArr } as PasswordValidator
 }
