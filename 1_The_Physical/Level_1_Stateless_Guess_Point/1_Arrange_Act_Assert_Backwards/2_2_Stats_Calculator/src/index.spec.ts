b6cf5e8faa084985d8cdef53bdaaafa40ec3fa31
Matt Wallin <matt.wallin2@gmail.com>
2023-09-26 15:14:42 -0700

    Initial commit for Stats Calc

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,10 @@
+import statsCalc from "./index"
 
 describe('stats calculator', () => {
-
+    it.each([
+        [[1, 2]],
+      ])('knows that %j ', (intArr) => {
+        expect(statsCalc(intArr)).toBeTruthy();
+      });
 
 })
---
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,5 @@
+
+
+export default function statsCalc(intArr: number[]): boolean {
+    return true
+}
